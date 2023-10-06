c80cd3fd5bd943bbdd4cbdcc8e193a6e51a24c13
Yevgeniy Davydov <yevgeniydavydov@Yevgeniys-MBP.fritz.box>
2023-10-06 11:45:15 +0200

    homework added

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict'
+
+// 1 Task//
+
+// const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+
+// // let user1 = userNames[0];
+// // let user2 = userNames[1];
+// // let user3 = userNames[2];
+
+// // const initials = [user2, user1, user3]
+
+// // console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
+
+
+//1 task - second try
+
+// const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
+// const copyUserNames = userNames.slice();
+
+// const userName = copyUserNames.map((name, index)=> {
+//     return name + index;
+// })
+
+
+// console.log(copyUserNames) //  и тут я сдался...
+
+
+
+// 2 Task
+
+// const userNames = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+// let filteredNames = userNames.filter((name) =>{
+//     return name.index === ("А", "Е", "I", "О", "У", "Я");
+// });
+
+
+
+// console.log(filteredNames); // ['Емма', 'Юстин', 'Ілля', 'Яна', 'Антон', 'Олена']
+
+
+
+
+// 3 Task//
+
+// const currentMaxValue = 4589;
+// const arr = [];
+// arr.unshift(currentMaxValue);
+// const copiedArr = arr.slice();
+// const reverseArr = copiedArr.reverse();
+
+// let reverseMaxValue = reverseArr.join();
+// // const reverseMaxValue = number;
+
+
+
+// console.log(reverseMaxValue); // 9854
+// console.log(typeof reverseMaxValue); // 'number'
+
+
+
+
+// 4 Task
+
+
+// const resultsArray = [1, 2, [3, [4]]];
+// const flatArr = resultsArray.flat(Infinity);
+// const mathArr = flatArr.Math((index0 + index1 + index2) * index3);
+// let productOfArray = mathArr.join('');
+
+
+// console.log(productOfArray); // 24
+
